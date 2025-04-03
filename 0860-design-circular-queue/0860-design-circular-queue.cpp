36da3028fe2a968509fffe0b2c923ed1175dde5a
Shivam Bhosale <71651684+ShivamB10@users.noreply.github.com>
2025-04-03 18:25:52 -0400

    Time: 5 ms (32.35%), Space: 23.4 MB (92.1%) - LeetHub

---
+++ 0860-design-circular-queue/0860-design-circular-queue.cpp
@@ -0,0 +1,89 @@
+class MyCircularQueue {
+private:
+    int size = 0;
+    ListNode* head;
+    ListNode* tail;
+    int limit = 0;
+public:
+    MyCircularQueue(int k) {
+        limit = k;
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
+    
+    bool enQueue(int value) {
+        if(isFull()){
+            return false;
+        }
+        
+        ListNode* curr = new ListNode(value);
+
+        if(isEmpty()){
+            head = tail = curr;
+            tail->next = head;
+        }
+        else{
+            tail->next = curr;
+            tail = curr;
+            tail->next = head;
+        }
+
+        size++;
+        return true;
+        
+    }
+    
+    bool deQueue() {
+        if(isEmpty()){
+            return false;
+        }
+
+        if(head == tail){
+            delete head;
+            head = tail = nullptr;
+        }
+        else{
+            ListNode* temp = head;
+            head = head->next;
+            tail->next = head;
+            delete temp;
+        }
+        size--;
+        return true;
+    }
+    
+    int Front() {
+        if(isEmpty()){
+            return -1;
+        }
+
+        return head->val;
+    }
+    
+    int Rear() {
+        if(isEmpty()){
+            return -1;
+        }
+        return tail->val;
+    }
+    
+    bool isEmpty() {
+        return size ==0;
+    }
+    
+    bool isFull() {
+        return size == limit;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
