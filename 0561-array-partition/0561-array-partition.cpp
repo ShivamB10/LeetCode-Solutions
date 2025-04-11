30a457042b6ecdb1849ff77734fe9a9acc5dedf4
Shivam Bhosale <71651684+ShivamB10@users.noreply.github.com>
2025-04-11 11:17:54 -0400

    Time: 14 ms (34.51%), Space: 31.9 MB (98.48%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int total = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            total += nums[i];
+            i++;
+        }
+
+        return total;
+    }
+};
